06825ab91008856764f94a11e5930c91af7ec41f
Sandro Flausino <sjflausino@gmail.com>
2025-07-15 09:50:21 -0300

    add version on required_providers

--- versions.tf
+++ versions.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
     aws = {
+      version = "~> 6.2.0"
       source  = "hashicorp/aws"
     }
     ansible = {
+      version = "~> 1.3.0"
       source  = "ansible/ansible"
     }
   }
